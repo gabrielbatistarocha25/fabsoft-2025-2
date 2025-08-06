b4134cfdc58630875650eda30084aefd515cd052
Gabriel Rocha - Mailou.Cloud <gabriel.rocha@mailou.com.br>
2025-08-05 22:01:01 -0300

    Revisão de Java e POO

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # fabsoft-2025-2
+
+## Aula 05/08
+
+- JRE - Java Runtime Enviroment
+    - Ambiente mínimo para executar um programa Java
+    - JVM - Java Virtual Machine (java.exe ou javaw.exe)
+
+- JDK - Java Development Kit
+    - Ambiente de desenvolvimento (javac.exe) compilador
+
+- COMPILAÇÃO
+    1) Escreve um programa em Java (arquivo.java)
+    2) Compilação arquivo.java -> javac.exe -> bytecode arquivo.class(funciona em qualquer SO)
+
+- EXECUÇÃO
+    3) Passar .class -> java.exe (JVM) -> linguagem de máquina
---
+++ revisao-poo/src/main/java/br/univille/entity/Cliente.java
@@ -0,0 +1,26 @@
+package br.univille.entity;
+
+public class Cliente {
+    private String nome;
+    private int idade;
+    private float peso; //private é o modificador de acesso
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+}
